cde0491637c22eedc301ca679215e0f93919df10
vamseeuxd <vamsee.uxd@gmail.com>
2023-08-22 13:16:40 +0530

    done with CanActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,49 +1,59 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthService } from './services/auth/auth.service';
 
 const routes: Routes = [
   {
     path: 'franchise',
+    canActivate: [AuthService],
     loadChildren: () =>
       import('./pages/franchise/franchise.module').then(m => m.FranchiseModule),
   },
   {
     path: 'brachces',
+    canActivate: [AuthService],
     loadChildren: () =>
       import('./pages/brachces/brachces.module').then(m => m.BrachcesModule),
   },
   {
     path: 'products',
+    canActivate: [AuthService],
     loadChildren: () =>
       import('./pages/products/products.module').then(m => m.ProductsModule),
   },
   {
     path: 'prices',
+    canActivate: [AuthService],
     loadChildren: () =>
       import('./pages/prices/prices.module').then(m => m.PricesModule),
   },
   {
     path: 'inventory',
+    canActivate: [AuthService],
     loadChildren: () =>
       import('./pages/inventory/inventory.module').then(m => m.InventoryModule),
   },
   {
     path: 'sales',
+    canActivate: [AuthService],
     loadChildren: () =>
       import('./pages/sales/sales.module').then(m => m.SalesModule),
   },
   {
     path: 'vendors',
+    canActivate: [AuthService],
     loadChildren: () =>
       import('./pages/vendors/vendors.module').then(m => m.VendorsModule),
   },
   {
     path: 'cutomers',
+    canActivate: [AuthService],
     loadChildren: () =>
       import('./pages/cutomers/cutomers.module').then(m => m.CutomersModule),
   },
   {
     path: 'order-taking',
+    canActivate: [AuthService],
     loadChildren: () =>
       import('./pages/order-taking/order-taking.module').then(
         m => m.OrderTakingModule
---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService implements CanActivate {
+  constructor() {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return of(false);
+  }
+}
