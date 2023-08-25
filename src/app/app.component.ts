51e4f1cb0509decc2d57fdb2cfa2389002f510d7
vamseeuxd <vamsee.uxd@gmail.com>
2023-08-25 11:23:46 +0530

    fix: pwa update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(swUpdate: SwUpdate) {
+    swUpdate.versionUpdates
+      .pipe(
+        filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY')
+      )
+      .subscribe(() => {
+        if (confirm('update available for the app please conform')) {
+          // Reload the page to update to the latest version.
+          document.location.reload();
+        }
+      });
+  }
+}
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -5,9 +5,18 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutingModule, MatIconModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    LoginRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    MatCardModule,
+  ],
 })
 export class LoginModule {}
